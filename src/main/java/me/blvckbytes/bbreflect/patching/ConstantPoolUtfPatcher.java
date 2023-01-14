4a526bbd25116e85ad319e282bf196e1c647ad99
Philip Hell <blvckbytes@gmail.com>
2023-01-14 19:40:28 +0100

    added 1.7 netty support by writing a custom class-loader as well as a byte-code patcher in order to adapt netty imports; the ReflectionHelperFactory is now being used as a means to construct these patched instances

---
+++ src/main/java/me/blvckbytes/bbreflect/patching/ConstantPoolUtfPatcher.java
@@ -0,0 +1,177 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 BlvckBytes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.blvckbytes.bbreflect.patching;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+
+public class ConstantPoolUtfPatcher {
+
+  private final Function<String, String> utfPatcher;
+
+  /**
+   * Create a new patcher to patch all utf-8 entries of the constant
+   * pool of a class represented as byte-code. See
+   * <a href="https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html">specification</a>
+   * @param utfPatcher External patcher function, invoked on every utf-8 element
+   */
+  public ConstantPoolUtfPatcher(Function<String, String> utfPatcher) {
+    this.utfPatcher = utfPatcher;
+  }
+
+  /**
+   * Patch the byte-code of a given class
+   * @param inputStream Input stream of data
+   * @param outputStream Output stream of data
+   */
+  public void patch(DataInputStream inputStream, OutputStream outputStream) throws IOException {
+    DataOutputStream output = new DataOutputStream(outputStream);
+
+    // magic
+    output.write(inputStream.read());
+    output.write(inputStream.read());
+    output.write(inputStream.read());
+    output.write(inputStream.read());
+
+    // major, minor
+    output.writeShort(inputStream.readShort());
+    output.writeShort(inputStream.readShort());
+
+    int constantPoolCount = inputStream.readShort();
+    output.writeShort(constantPoolCount);
+
+    // Minus one, because... because!
+    for (int i = 0; i < constantPoolCount - 1; i++)
+      patchCpInfo(inputStream, output);
+
+    // Write remaining bytes
+    int read;
+    while ((read = inputStream.read()) != -1)
+      output.write((byte) read);
+  }
+
+  /**
+   * Sub-routine used to to patch a cp-info struct (constant pool entry)
+   */
+  private void patchCpInfo(DataInputStream inputStream, DataOutputStream outputStream) throws IOException {
+    byte tag = inputStream.readByte();
+    outputStream.write(tag);
+
+    switch (tag) {
+      // CONSTANT_Class
+      case 7:
+        // name_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_Fieldref
+      case 9:
+      // CONSTANT_Methodref
+      case 10:
+      // CONSTANT_InterfaceMethodref
+      case 11:
+        // class_index
+        outputStream.writeShort(inputStream.readShort());
+        // name_and_type_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_String
+      case 8:
+        // string_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_Integer
+      case 3:
+      // CONSTANT_Float
+      case 4:
+        // bytes
+        outputStream.writeInt(inputStream.readInt());
+        break;
+
+      // CONSTANT_Long
+      case 5:
+      // CONSTANT_Double
+      case 6:
+        // high_bytes
+        outputStream.writeInt(inputStream.readInt());
+        // low_bytes
+        outputStream.writeInt(inputStream.readInt());
+        break;
+
+      // CONSTANT_NameAndType
+      case 12:
+        // name_index
+        outputStream.writeShort(inputStream.readShort());
+        // descriptor_ref
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_Utf8
+      case 1:
+        // length
+        short length = inputStream.readShort();
+
+        byte[] data = new byte[length];
+
+        if (inputStream.read(data) != length)
+          throw new IllegalStateException("Reading UTF8 data failed");
+
+        byte[] patchResult = utfPatcher.apply(new String(data)).getBytes(StandardCharsets.UTF_8);
+
+        // Write new length and contents
+        outputStream.writeShort(patchResult.length);
+        outputStream.write(patchResult);
+        break;
+
+      // CONSTANT_MethodHandle
+      case 15:
+        // reference_kind
+        outputStream.write(inputStream.read());
+        // reference_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_MethodType
+      case 16:
+        // descriptor_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      // CONSTANT_InvokeDynamic
+      case 18:
+        // bootstrap_method_attr_index
+        outputStream.writeShort(inputStream.readShort());
+        // name_and_type_index
+        outputStream.writeShort(inputStream.readShort());
+        break;
+
+      default:
+        throw new IllegalStateException("Unknown tag encountered: " + tag);
+    }
+  }
+}
---
+++ src/main/java/me/blvckbytes/bbreflect/patching/UtfPatcherClassLoader.java
@@ -0,0 +1,136 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 BlvckBytes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.blvckbytes.bbreflect.patching;
+
+import java.io.*;
+import java.net.URL;
+import java.util.function.Function;
+
+public class UtfPatcherClassLoader extends ClassLoader {
+
+  // FIXME: Is there a way to clear cache onDisable of the plugin?
+
+  private final ConstantPoolUtfPatcher patcher;
+  private final Function<String, Boolean> patchChecker;
+
+  /**
+   * Create a new class loader which will invoke an external utf patcher for every entry in
+   * the constant pool of loaded classes which match on the passed path checker predicate
+   * @param parent Parent class loader
+   * @param patchChecker Path checker predicate which decides which class needs patching
+   * @param utfPatcher Utf patcher function, invoked for every utf constant of every patched class
+   */
+  public UtfPatcherClassLoader(
+    ClassLoader parent,
+    Function<String, Boolean> patchChecker,
+    Function<String, String> utfPatcher
+  ) {
+    super(parent);
+
+    this.patcher = new ConstantPoolUtfPatcher(utfPatcher);
+    this.patchChecker = patchChecker;
+  }
+
+  @Override
+  protected Class<?> findClass(String name) throws ClassNotFoundException {
+    try {
+      String path = name.replace('.', '/') + ".class";
+
+      // If this class doesn't need patching it's not handled and the request
+      // is being bubbled up to the parent by the CNFE
+      if (!this.patchChecker.apply(normalizeName(name)))
+        throw new ClassNotFoundException();
+
+      // Try to load the requested resource at the requested path
+      URL resource = getResource(path);
+      if (resource == null)
+        throw new ClassNotFoundException();
+
+      try (
+        InputStream is = resource.openStream();
+        DataInputStream input = new DataInputStream(is);
+        ByteArrayOutputStream output = new ByteArrayOutputStream()
+      ) {
+        // Run the class' byte-code through the patcher before passing it
+        // to the byte-code reader to be turned into an actual class
+        this.patcher.patch(input, output);
+        byte[] bytes = output.toByteArray();
+        return defineClass(name, bytes, 0, bytes.length);
+      }
+    }
+
+    // Don't add unnecessary layers to a CNFE
+    catch (ClassNotFoundException e) {
+      throw e;
+    }
+
+    // Transform internal exceptions into CNFE with a suppressed inner exception
+    catch (Throwable t) {
+      t.printStackTrace();
+      ClassNotFoundException exception = new ClassNotFoundException("Internal error while loading class " + name);
+      exception.addSuppressed(t);
+      throw exception;
+    }
+  }
+
+  @Override
+  protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+    Class<?> c = findLoadedClass(name);
+
+    if (c != null) {
+      if (resolve)
+        resolveClass(c);
+      return c;
+    }
+
+    try {
+      c = findClass(name);
+      if (resolve)
+        resolveClass(c);
+      return c;
+    } catch (Throwable t) {
+      try {
+        return super.loadClass(name, resolve);
+      } catch (ClassNotFoundException cnfe) {
+        cnfe.addSuppressed(t);
+        throw cnfe;
+      }
+    }
+  }
+
+  /**
+   * Normalizes a name to only contain the root class, which means
+   * member class notation is being stripped off
+   */
+  private String normalizeName(String name) {
+    // If there's a member in the target class, I of course also want it patched.
+    int firstIndex = name.indexOf('$');
+
+    if (firstIndex < 0)
+      return name;
+
+    return name.substring(0, firstIndex);
+  }
+}
